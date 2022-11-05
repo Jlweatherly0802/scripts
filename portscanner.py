f58c1e64e548c459e772d5c84d660a20fee31683
jay-boy <jay-boy@pop-os.localdomain>
2022-11-04 21:27:05 -0700

    base

---
+++ portscanner.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+import queue
+
+q = queue.Queue()
+
+target = input('Target: ')
+ports = input('Max number of ports: ')
+
+for x in range(1,int(ports)):
+    q.put(x)
+
+def Pscan(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn = s.connect((target, port))
+        return True 
+
+    except:
+        return False 
+
+def worker():
+    while True:
+        port = q.get()
+        if Pscan(port):
+            print('Port {} open' .format(port))
+        elif q.empty():
+            break
+
+        #else:
+         #   print('Port {} closed' .format(port))
+
+for x in range(30):
+    t = threading.Thread(target=worker)
+    t.start()
